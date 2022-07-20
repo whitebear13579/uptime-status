78328f1888ae3481351a81eac6516bd339c8a435
whitebear13579 <68762923+whitebear13579@users.noreply.github.com>
2022-07-20 18:10:38 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,36 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '系統狀態 | System Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm792290797-986e20b6d822e9f029c74858',
+    'm792290804-e97e2d66eac0375e90372022',
+    'm792290824-f2ae5c564fdf2230f1b52aa9',
+    'm792290816-9f04f9c6cafbd075d49a9d2b',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'HomePage',
+      url: 'https://status.ahhyouok.net'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/whitebear13579'
+    },
+    {
+      text: 'Blog',
+      url: 'https://blog.coolyeah.ga/'
+    },
+  ],
+};
